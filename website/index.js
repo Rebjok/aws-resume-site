922c5f60a18cf21c6952415b66b03fa4066c2edc
Rebi.boy <rebjok911@gmail.com>
2023-11-01 20:38:24 +0000

    changes to html

--- website/index.js
+++ website/index.js
@@ -2,6 +2,6 @@ const counter = document.querySelector(".counter-number")
 async function updateCounter(){
     let response = await fetch("https://a23p6gy35ux2427qhbsuuijf7u0rubki.lambda-url.eu-west-2.on.aws/")
     let data = await response.json()
-    counter.innerHTML = `Views: ${data}`
+    counter.innerHTML = `Number of Views: ${data}`
 }
 updateCounter()
